c0b0db9453911638bab92d74c2fef6dce325d70e
Bridget Black <67387913+BlackHyena12@users.noreply.github.com>
2021-03-21 17:02:51 -0700

    Started design layout portrait/landscape
    **What Was Done**
    - Init project
    - Init portrait design
    - Init landscape design
    
    **Needs To Be Done**
    - Warnings on all layout views addressed
    - All calculation and display functionalities created

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,9 +3,17 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Calculator with basic arithmetic operation capacity.
+ *
+ * @author Bridget Black
+ * 2021-03-21
+ * Last Updated: 2021-03-21
+ */
 class MainActivity : AppCompatActivity() {
-
-
+    /**
+     * Create saved state. TODO update description with added functionality
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
